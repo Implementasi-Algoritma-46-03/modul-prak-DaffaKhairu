2abe3d52f8f4718d682d041833ad78f6308384f0
DaffaKhairu <bocahrentalps@gmail.com>
2025-10-02 18:25:15 +0700

    Mengumpulkan jurnal

--- Modul03/Jurnal01.java
+++ Modul03/Jurnal01.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner inputScanner = new Scanner(System.in);
+        int angka = inputScanner.nextInt();
+        int nomor = angka / 1000;
+        int sisa = angka % 1000;
+
+        System.out.println(nomor);
+        System.out.println(sisa);
+        inputScanner.close();
     }
 }
--- Modul03/Jurnal03.java
+++ Modul03/Jurnal03.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner (System.in);
+        int nilaiP = input.nextInt();
+        int nilaiA = input.nextInt();
+        int nilaiB = input.nextInt();
+
+        double NA = (0.25* nilaiP + 0.35 * nilaiA + 0.40 * nilaiB); 
+        System.out.printf("%.2f\n", NA);
+        System.out.printf("Lulus MK: " + (NA >=75) );
+        
+
     }
 }
