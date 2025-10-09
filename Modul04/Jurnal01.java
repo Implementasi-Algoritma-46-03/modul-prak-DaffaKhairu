74a3b48c72613ad6224a58d72c448908bb79ef0d
DaffaKhairu <bocahrentalps@gmail.com>
2025-10-09 17:49:26 +0700

    Jurnal Modul04

--- Modul04/Jurnal01.java
+++ Modul04/Jurnal01.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+
+        if (24 % n == 0 && 56 % n == 0) {
+            System.out.println("Faktor dari 24 dan 56");
+        }else {
+            System.out.println("Bukan Faktor dari 24 dan 56");
+        }
+        
     }
 }
--- Modul04/Jurnal03.java
+++ Modul04/Jurnal03.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+          Scanner s = new Scanner(System.in);
+        int Dira = s.nextInt();
+        int Radi = s.nextInt();
+        int Idar = s.nextInt();
+
+        if (Dira <= Radi && Radi<= Idar) {
+            System.out.println("Dira, Radi, Idar");           
+        }else if (Radi<=Idar && Idar<=Dira){
+         System.out.println("Radi, Idar, Dira");   
+        }else if (Idar<=Dira && Dira<=Radi) {
+            System.out.println("Idar, Dira, Radi");
+            
+        }
     }
 }
